b327dc39a102213417360ef8f32fde69e9be3bd1
Davies Fatimah Omowonuola <daviesfatimah97@gmail.com>
2021-10-10 11:06:39 +0100

    google docs icon

--- pages/components/Login.js
+++ pages/components/Login.js
@@ -5,7 +5,11 @@ import { signIn } from "next-auth/client";
 function Login() {
   return (
     <div className="flex flex-col items-center justify-center py-2 min-h-screen">
-      <h1>Google Docs</h1>
+      <img
+        src="https://res.cloudinary.com/dycyotaya/image/upload/v1633860160/PinClipart.com_clip-art-for-google_3789173_osywa9.png"
+        alt=""
+        style={{ width: "200px", height: "200px", margin: 0 }}
+      />
       <Button
         className="w-44 mt-10"
         color="blue"
